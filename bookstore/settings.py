5e37e2beae0b2395ba29a12598ad016195e1b731
xenacode-art <kaja.obinna@lmu.edu.ng>
2023-05-31 09:23:28 -0400

    rm all package dependency to build docker image

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k42k_wc7a@%-eh5u+mw-x0q!zi3zn14j%nlidew@2ibsoqr(fa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'books',
 ]
 
 MIDDLEWARE = [
