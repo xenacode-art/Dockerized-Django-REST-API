882e983deb53b1b67c72f8c6f8675a279f9f4cbe
xenacode-art <kaja.obinna@lmu.edu.ng>
2023-06-01 10:51:41 -0400

    docker image working perfectly and pushed to dockerhub

--- books/models.py
+++ books/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=20)
+    summary = models.TextField()
--- bookstore/settings.py
+++ bookstore/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-k42k_wc7a@%-eh5u+mw-x0q!zi3zn14j%nlidew@2ibsoqr(fa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', 
+                  '127.0.0.1']
 
 
 # Application definition
